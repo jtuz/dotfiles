78fa502f0a980c80c4caab332a7cb6206f4d6180
Jacob Tuz Poot <jetp79@proton.me>
2023-01-18 21:21:55 -0600

    feat: add translation language and platform info on statusline

--- nvchad/custom/ui/statusline.lua
+++ nvchad/custom/ui/statusline.lua
@@ -13,6 +13,7 @@ local default_sep_icons = {
 local separators = (type(sep_style) == "table" and sep_style) or default_sep_icons[sep_style]
 
 local sep_l = separators["left"]
+local sep_r = separators["right"]
 
 local navic = function ()
   if vim.o.columns < 140 or not package.loaded["nvim-navic"] then
@@ -22,19 +23,40 @@ local navic = function ()
   return (navic.is_available() and navic.get_location()) or ""
 end
 
+local platform = function ()
+  local os_icon = " "
+  if op_sys.LINUX() then
+    os_icon = " "
+  elseif op_sys.OSX() then
+    os_icon = " "
+  end
+
+  return  "%#St_cwd_text#" .. os_icon
+end
+
+local lang_translation = function ()
+  local spell = vim.api.nvim_get_option_value("spell", {})
+  local spelllang = vim.api.nvim_get_option_value("spelllang", {})
+
+  if not spell then
+    return ""
+  end
+  return "%#St_file_info#" .. " 暈" .. spelllang .. "%#St_file_sep#" .. sep_r
+end
+
 return {
+    fileInfo = function ()
+      return modules.fileInfo() .. lang_translation()
+    end,
     LSP_progress = function()
       return modules.LSP_progress() .. navic()
     end,
+    cwd = function ()
+      return modules.cwd() .. " " .. platform() .. " "
+    end,
     cursor_position = function()
-      local os_icon = ""
-      if op_sys.LINUX() then
-        os_icon = " "
-      elseif op_sys.OSX() then
-        os_icon = " "
-      end
-      local left_sep = "%#St_pos_sep#" .. sep_l .. "%#St_pos_icon#" .. os_icon
+      local left_sep = "%#St_pos_sep#" .. sep_l .. "%#St_pos_icon#" .. " "
       local text = vim.o.columns > 140 and "%l:%c" or ""
       return left_sep .. "%#St_Pos_txt# " .. text .. " "
-    end
+    end,
 }
