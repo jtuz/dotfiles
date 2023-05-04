ef7b5cbc5105439ec8530dea97a785216515ac56
Jacob Tuz Poot <jetp79@proton.me>
2023-05-04 10:36:50 -0600

    chore(lspconfig): enhance settings per language

--- nvchad/custom/configs/lspconfig.lua
+++ nvchad/custom/configs/lspconfig.lua
@@ -1,39 +1,24 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 local navic = require("nvim-navic")
 
-local servers = { "pyright", "gopls", "bashls", "jsonls", "marksman", "html" }
 
-
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = function (client, bufnr)
-      on_attach(client, bufnr)
-      navic.attach(client, bufnr)
-    end,
-    capabilities = capabilities,
-    handlers = {
-      ["textDocument/publishDiagnostics"] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {
-        -- Disable virtual_text
-        virtual_text = false
-      }
-      ),
-    },
-    -- root_dir = vim.loop.cwd,
-    flags = {
-      debounce_text_changes = 150,
-    },
+local servers = {
+  pyright = {
     settings = {
       python = {
         analysis = {
           autoSearchPaths = false,
           useLibraryCodeForTypes = false,
-          diagnosticMode = 'openFilesOnly',
-        }
+          diagnosticMode = "openFilesOnly",
+        },
       },
+    },
+  },
+  gopls = {
+    settings = {
       gopls = {
         analyses = {
           unusedparams = true,
@@ -41,8 +26,34 @@ for _, lsp in ipairs(servers) do
         staticcheck = true,
       },
     },
+  },
+  bashls = {},
+  jsonls = {},
+  marksman = {},
+  html = {},
+}
+
+for server, config in pairs(servers) do
+  lspconfig[server].setup {
+    on_attach = function(client, bufnr)
+      on_attach(client, bufnr)
+      navic.attach(client, bufnr)
+    end,
+    capabilities = capabilities,
+    handlers = {
+      ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+        -- Disable virtual_text
+        virtual_text = false,
+      }),
+    },
+    -- root_dir = vim.loop.cwd,
+    flags = {
+      debounce_text_changes = 150,
+    },
+    settings = config.settings,
   }
 end
+
 -- custom typescript  example
 -- lspconfig.tsserver.setup {
 --   cmd = { "typescript-language-server", "--stdio" },
