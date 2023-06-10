8590ca6e59b4fa7fc5685d4f511999d7859f0e91
Jacob Tuz Poot <jetp79@proton.me>
2023-06-10 17:26:55 -0600

    chore(nvchad): Set cmake as builder for telescope-fzf-native

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -134,7 +134,7 @@ return {
   },
   {
     "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
     dependencies = {
       { "nvim-telescope/telescope.nvim" },
     },
