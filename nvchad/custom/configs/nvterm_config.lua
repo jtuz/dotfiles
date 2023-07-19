d3bd966e16256cac712ab4caefe3253878e984ed
Jacob Tuz Poot <jetp79@proton.me>
2023-07-19 16:34:33 -0600

    chore(nvchad): set mapping key for run tests with nvterm

--- nvchad/custom/configs/nvterm_config.lua
+++ nvchad/custom/configs/nvterm_config.lua
@@ -1,24 +1,20 @@
 local terminal = require("nvterm.terminal")
-local toggle_modes = {"n", "t"}
 
-local function get_command()
-  local current_file = vim.fn.expand('%:.')
-  return "make testnm ARG="..current_file
-end
+local toggle_modes = { "n", "t" }
+local opts = { noremap = true, silent = true }
 
-local ft_cmds = {
-  python = get_command()
-}
-
-
-local mappings = {
-  { "n", "<leader>ts", function () terminal.send(ft_cmds[vim.bo.filetype], "float") end },
-  { toggle_modes, "<leader>tf", function () terminal.toggle("float") end },
-  { toggle_modes, "<leader>tv", function () terminal.toggle("vertical") end },
-  { toggle_modes, "<leader>tz", function () terminal.toggle("horizontal") end },
-}
-
-local opts = { noremap = true, silent=true }
-for _, mapping in pairs(mappings) do
-  vim.keymap.set(mapping[1], mapping[2], mapping[3], opts)
-end
+vim.keymap.set("n", "<leader>ts", function()
+	local ft_cmds = {
+		python = "make testnm ARG=" .. vim.fn.expand("%"),
+	}
+	terminal.send(ft_cmds[vim.bo.filetype])
+end, opts)
+vim.keymap.set(toggle_modes, "<leader>tf", function()
+	terminal.toggle("float")
+end, opts)
+vim.keymap.set(toggle_modes, "<leader>tv", function()
+	terminal.toggle("vertical")
+end, opts)
+vim.keymap.set(toggle_modes, "<leader>tz", function()
+	terminal.toggle("horizontal")
+end, opts)
