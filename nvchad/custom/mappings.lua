9e5141aded78df15bd3a35367da19c81432a2f1d
Jacob Tuz Poot <jetp79@proton.me>
2024-03-03 20:43:10 -0600

    chore(nvchad): change mapping format

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -1,159 +1,94 @@
-local M = {}
+local map = vim.keymap.set
 
-M.disabled = {
-  n = {
-    ["<C-s>"] = "",
-    ["<C-n>"] = "",
-    ["<C-c>"] = "",
-    ["<leader>b"] = "",
-    ["<leader>gt"] = "",
-    -- Disable NvTerm shortcuts
-    ["<A-i>"] = "",
-    ["<A-h>"] = "",
-    ["<A-v>"] = "",
-    ["<leader>h"] = "",
-    ["<leader>v"] = "",
-  },
-  t = {
-    ["<A-i>"] = "",
-    ["<A-h>"] = "",
-    ["<A-v>"] = "",
-  }
-}
+map("n", "<C-s>", "<NOP>")
+map("n", "<C-n>", "<NOP>")
+map("n", "<C-c>", "<NOP>")
+map("n", "<leader>b", "<NOP>")
+map("n", "<leader>gt", "<NOP>")
+-- Disable NvTerm shortcuts
+map("n", "<A-i>", "<NOP>")
+map("n", "<A-h>", "<NOP>")
+map("n", "<A-v>", "<NOP>")
+map("n", "<leader>h", "<NOP>")
+map("n", "<leader>v", "<NOP>")
+map("t", "<A-i>", "<NOP>")
+map("t", "<A-h>", "<NOP>")
+map("t", "<A-v>", "<NOP>")
 
-M.telescope = {
-  n = {
-    ["<leader>sy"] = { "<cmd> Telescope lsp_document_symbols<CR>", "Find document symbols" },
-    ["<leader>gr"] = { "<cmd> Telescope lsp_references<CR>","Find document references" },
-    ["<leader>dg"] = { "<cmd> Telescope diagnostics<CR>","Document diagnostics" },
-    ["<leader>fg"] = { "<cmd> Telescope git_files<CR>", "Find git files" },
-    ["<leader>gs"] = { "<cmd> Telescope git_status<CR>", "Git status" },
-    ["<leader>fp"] = { "<cmd> Telescope media_files<CR>", "Find media" },
-  },
-}
+-- Telescope
+map("n", "<leader>sy", "<cmd> Telescope lsp_document_symbols<CR>", { desc = "Find document symbols" })
+map("n", "<leader>gr", "<cmd> Telescope lsp_references<CR>", { desc = "Find document references" })
+map("n", "<leader>dg", "<cmd> Telescope diagnostics<CR>", { desc = " Document diagnostics" })
+map("n", "<leader>fg", "<cmd> Telescope git_files<CR>", { desc = "Find git files" })
+map("n", "<leader>gs", "<cmd> Telescope git_status<CR>", { desc = "Git status" })
+map("n", "<leader>fp", "<cmd> Telescope media_files<CR>", { desc = "Find media" })
 
-M.nvimtree = {
-   n = {
-    ["<leader>ft"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
-   },
-}
+-- Nvim tree
+map("n", "<leader>ft", "<cmd> NvimTreeToggle <CR>", { desc = "toggle nvimtree" })
 
-M.spelling = {
-  n = {
-    ["<F2>"] = { "<Esc><cmd> setlocal spell spelllang=en<CR>", "ENG spelling" },
-    ["<F3>"] = { "<Esc><cmd> setlocal spell spelllang=es<CR>", "ESP spelling" },
-    ["<F4>"] = { "<Esc><cmd> setlocal nospell<CR>", "Disable spelling" },
-  },
-}
+-- Spell
+map("n", "<F2>", "<Esc><cmd> setlocal spell spelllang=en<CR>", { desc = "ENG spelling" })
+map("n", "<F3>", "<Esc><cmd> setlocal spell spelllang=es<CR>", { desc = "ESP spelling" })
+map("n", "<F4>", "<Esc><cmd> setlocal nospell<CR>", { desc = "Disable spelling" })
 
-M.fugitive = {
-  n = {
-    ["<leader>gb"] = { "<cmd> Git blame <CR>", "git blame" },
-    ["<leader>gh"] = { "<cmd> diffget //2 <CR>", "diffget //2" },
-    ["<leader>gl"] = { "<cmd> diffget //3 <CR>", "diffget //3" },
-  },
-}
+-- fugitive
+map("n", "<leader>gb", "<cmd> Git blame <CR>", { desc = "git blame" })
+map("n", "<leader>gh", "<cmd> diffget //2 <CR>", { desc = "diffget //2" })
+map("n", "<leader>gl", "<cmd> diffget //3 <CR>", { desc = "diffget //3" })
 
-M.misc = {
-  n = {
-    -- Vim Ninja Skills
-    ["<Up>"] = { "<NOP>", "Disable Up" },
-    ["<Down>"] = { "<NOP>", "Disaple Down" },
-    ["<Left>"] = { "<NOP>", "Disable Left" },
-    ["<Right>"] = { "<NOP>", "Disable Right" },
-    -- Personal Tools
-    ["<leader>jq"] = { "<Esc><cmd>%!jq .<CR><Esc><cmd> set filetype=json<CR>", "Format Json" },
-    ["<leader>so"] = { "<cmd> SymbolsOutline<CR>", "Symbols Outline" }
-  },
+-- Vim Ninja Skills
+map("n", "<Up>", "<NOP>", { desc = "Disable Up" })
+map("n", "<Down>", "<NOP>", { desc = "Disaple Down" })
+map("n", "<Left>", "<NOP>", { desc = "Disable Left" })
+map("n", "<Right>", "<NOP>", { desc = "Disable Right" })
+-- Personal Tools
+map("n", "<leader>jq", "<Esc><cmd>%!jq .<CR><Esc><cmd> set filetype=json<CR>", { desc = "Format Json" })
+map("n", "<leader>so", "<cmd> SymbolsOutline<CR>", { desc = "Symbols Outline" })
 
-  i = {
-    -- Vim Ninja Skills
-    ["<Up>"] = { "<NOP>", "Disable Up" },
-    ["<Down>"] = { "<NOP>", "Disable Down" },
-    ["<Left>"] = { "<NOP>", "Disable Left" },
-    ["<Right>"] = { "<NOP>", "Disable Right" },
-    -- navigate within insert mode                                                                              │    │
-    ["<C-h>"] = { "<Left>", "move left" },
-    ["<C-l>"] = { "<Right>", "move right" },
-    ["<C-j>"] = { "<Down>", "move down" },
-    ["<C-k>"] = { "<Up>", "move up" },
-  },
-}
+-- Vim Ninja Skills
+map("i", "<Up>", "<NOP>", { desc = "Disable Up" })
+map("i", "<Down>", "<NOP>", { desc = "Disable Down" })
+map("i", "<Left>", "<NOP>", { desc = "Disable Left" })
+map("i", "<Right>", "<NOP>", { desc = "Disable Right" })
+-- navigate within insert mode
+map("i", "<C-h>", "<Left>", { desc = "move left" })
+map("i", "<C-l>", "<Right>", { desc = "move right" })
+map("i", "<C-j>", "<Down>", { desc = "move down" })
+map("i", "<C-k>", "<Up>", { desc = "move up" })
 
-M.gitsigns = {
-  n= {
-    ["<leader>hb"] = {
-      function()
-        require("gitsigns").blame_line()
-      end,
-      "Blame line",
-    },
-  },
-}
+-- gitsigns
+map("n", "<leader>hb", function()
+  require("gitsigns").blame_line()
+end, { desc = "Blame line" })
 
-M.dap = {
-  n = {
-    ["<F5>"] = {
-      function()
-        require("dap").continue()
-      end,
-      "Start Debbuging"
-    },
-    ["<F10>"] = {
-      function()
-        require("dap").step_over()
-      end,
-      "Step Over",
-    },
-    ["<F11>"] = {
-      function()
-        require("dap").step_into()
-      end,
-      "Step Into"
-    },
-    ["<F12>"] = {
-      function()
-        require("dap").step_out()
-      end,
-      "Step Out"
-    },
-    ["<leader>b"] = {
-      function()
-        require("dap").toggle_breakpoint()
-      end,
-      "Toggle Breakpoint",
-    },
-    ["<leader>B"] = {
-      function()
-        require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-      end,
-      "Set Breakpoint Condition",
-    },
-    ["<leader>lp"] = {
-      function()
-        require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-      end,
-      "Log point message",
-    },
-    ["<leader>repl"] = {
-      function()
-        require("dap").repl.open()
-      end,
-      "Open REPL",
-    },
-    ["<leader>du"] = {
-      function()
-        require("dapui").toggle()
-      end,
-      "Toggle DAP GUI",
-    },
-    ["<leader>vl"] = {
-      function()
-        require("dapui").eval()
-      end,
-      "Evaluate expression",
-    },
-  }
-}
-
-return M
+-- DAP
+map("n", "<F5>", function()
+  require("dap").continue()
+end, { desc = "Start Debbuging" })
+map("n", "<F10>", function()
+  require("dap").step_over()
+end, { desc = "Step Over", })
+map("n", "<F11>", function()
+  require("dap").step_into()
+end, { desc = "Step Into" })
+map("n", "<F12>", function()
+  require("dap").step_out()
+end, { desc = "Step Out" })
+map("n", "<leader>b", function()
+  require("dap").toggle_breakpoint()
+end, { desc = "Toggle Breakpoint", })
+map("n", "<leader>B", function()
+  require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end, { desc = "Set Breakpoint Condition", })
+map("n", "<leader>lp", function()
+  require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+end, { desc = "Log point message", })
+map("n", "<leader>repl", function()
+  require("dap").repl.open()
+end, { desc = "Open REPL", })
+map("n", "<leader>du", function()
+  require("dapui").toggle()
+end, { desc = "Toggle DAP GUI", })
+map("n", "<leader>vl", function()
+  require("dapui").eval()
+end, { desc = "Evaluate expression", })
