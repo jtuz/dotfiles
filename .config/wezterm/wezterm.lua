da3e54eb53ee1406714c11c38a4a98f7df624ac3
Jacob E. Tuz Poot <jetp79@proton.me>
2024-09-05 09:51:13 -0600

    feat: add green blur theme for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,7 +10,8 @@ end
 config.use_fancy_tab_bar = false
 config.enable_scroll_bar = false
 -- config.color_scheme = "TokyoNight"
-config.color_scheme = 'Gruvbox dark, hard (base16)'
+config.color_scheme = "Gruvbox dark, hard (base16)"
+config.color_scheme = "Elementary"
 config.colors = {
 	tab_bar = {
 		active_tab = {
@@ -104,9 +105,9 @@ config.use_dead_keys = true
 config.audible_bell = "Disabled"
 config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-	{ key = "C",         mods = "CTRL",       action = act.CopyTo("Clipboard") },
-	{ key = "-",         mods = "CTRL",       action = act.DecreaseFontSize },
-	{ key = "=",         mods = "CTRL",       action = act.IncreaseFontSize },
+	{ key = "C", mods = "CTRL", action = act.CopyTo("Clipboard") },
+	{ key = "-", mods = "CTRL", action = act.DecreaseFontSize },
+	{ key = "=", mods = "CTRL", action = act.IncreaseFontSize },
 	{ key = "Backspace", mods = "CTRL|SHIFT", action = act.ResetFontSize },
 	{
 		key = "r",
@@ -129,72 +130,68 @@ config.keys = {
 }
 config.key_tables = {
 	resize_pane = {
-		{ key = "LeftArrow",  action = act.AdjustPaneSize({ "Left", 1 }) },
-		{ key = "h",          action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
 
 		{ key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
-		{ key = "l",          action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
 
-		{ key = "UpArrow",    action = act.AdjustPaneSize({ "Up", 1 }) },
-		{ key = "k",          action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "UpArrow", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
 
-		{ key = "DownArrow",  action = act.AdjustPaneSize({ "Down", 1 }) },
-		{ key = "j",          action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "DownArrow", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
 
 		-- Cancel the mode by pressing escape
-		{ key = "Escape",     action = "PopKeyTable" },
+		{ key = "Escape", action = "PopKeyTable" },
 	},
 }
 config.window_background_gradient = {
-  -- Can be "Vertical" or "Horizontal".  Specifies the direction
-  -- in which the color gradient varies.  The default is "Horizontal",
-  -- with the gradient going from left-to-right.
-  -- Linear and Radial gradients are also supported; see the other
-  -- examples below
-	orientation = 'Vertical',
-  -- orientation = { Linear = { angle = -45.0 } },
+	-- Can be "Vertical" or "Horizontal".  Specifies the direction
+	-- in which the color gradient varies.  The default is "Horizontal",
+	-- with the gradient going from left-to-right.
+	-- Linear and Radial gradients are also supported; see the other
+	-- examples below
+	orientation = "Vertical",
+	-- orientation = { Linear = { angle = -45.0 } },
 
-  -- Specifies the set of colors that are interpolated in the gradient.
-  -- Accepts CSS style color specs, from named colors, through rgb
-  -- strings and more
-  colors = {
-    '#0f0c29',
-    '#274060',
-    '#1B2845',
-  },
+	-- Specifies the set of colors that are interpolated in the gradient.
+	-- Accepts CSS style color specs, from named colors, through rgb
+	-- strings and more
+	colors = require("blur_themes").green,
 
-  -- Instead of specifying `colors`, you can use one of a number of
-  -- predefined, preset gradients.
-  -- A list of presets is shown in a section below.
-  -- preset = "Warm",
+	-- Instead of specifying `colors`, you can use one of a number of
+	-- predefined, preset gradients.
+	-- A list of presets is shown in a section below.
+	-- preset = "Warm",
 
-  -- Specifies the interpolation style to be used.
-  -- "Linear", "Basis" and "CatmullRom" as supported.
-  -- The default is "Linear".
-  -- interpolation = 'Linear',
+	-- Specifies the interpolation style to be used.
+	-- "Linear", "Basis" and "CatmullRom" as supported.
+	-- The default is "Linear".
+	-- interpolation = 'Linear',
 
-  -- How the colors are blended in the gradient.
-  -- "Rgb", "LinearRgb", "Hsv" and "Oklab" are supported.
-  -- The default is "Rgb".
-  -- blend = 'Rgb',
+	-- How the colors are blended in the gradient.
+	-- "Rgb", "LinearRgb", "Hsv" and "Oklab" are supported.
+	-- The default is "Rgb".
+	-- blend = 'Rgb',
 
-  -- To avoid vertical color banding for horizontal gradients, the
-  -- gradient position is randomly shifted by up to the `noise` value
-  -- for each pixel.
-  -- Smaller values, or 0, will make bands more prominent.
-  -- The default value is 64 which gives decent looking results
-  -- on a retina macbook pro display.
-  -- noise = 64,
+	-- To avoid vertical color banding for horizontal gradients, the
+	-- gradient position is randomly shifted by up to the `noise` value
+	-- for each pixel.
+	-- Smaller values, or 0, will make bands more prominent.
+	-- The default value is 64 which gives decent looking results
+	-- on a retina macbook pro display.
+	-- noise = 64,
 
-  -- By default, the gradient smoothly transitions between the colors.
-  -- You can adjust the sharpness by specifying the segment_size and
-  -- segment_smoothness parameters.
-  -- segment_size configures how many segments are present.
-  -- segment_smoothness is how hard the edge is; 0.0 is a hard edge,
-  -- 1.0 is a soft edge.
+	-- By default, the gradient smoothly transitions between the colors.
+	-- You can adjust the sharpness by specifying the segment_size and
+	-- segment_smoothness parameters.
+	-- segment_size configures how many segments are present.
+	-- segment_smoothness is how hard the edge is; 0.0 is a hard edge,
+	-- 1.0 is a soft edge.
 
-  -- segment_size = 11,
-  -- segment_smoothness = 0.0,
+	-- segment_size = 11,
+	-- segment_smoothness = 0.0,
 }
 
 return config
