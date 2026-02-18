57d7c5ea8f1a6057a8e800ca243903e010041a47
Jacob E. Tuz Poot <jetp79@proton.me>
2026-02-18 08:37:51 -0600

    Emergency backup

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,16 +11,19 @@ config.max_fps = 120
 config.prefer_egl = true
 
 -- Acceptable values are SteadyBlock, BlinkingBlock, SteadyUnderline, BlinkingUnderline, SteadyBar, and BlinkingBar
-config.default_cursor_style = "BlinkingBar"
+config.default_cursor_style = "BlinkingUnderline"
 config.enable_tab_bar = true
 config.show_tabs_in_tab_bar = true
 config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = true
 config.enable_scroll_bar = false
 config.window_decorations = "RESIZE" -- remove native window border
+config.adjust_window_size_when_changing_font_size = false
 -- config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 config.color_scheme = "TokyoNight"
+-- config.color_scheme = "nord"
 -- config.color_scheme = "Gruvbox dark, hard (base16)"
+-- config.color_scheme = 'Everforest Dark Hard (Gogh)'
 -- config.color_scheme = "Elementary"
 config.inactive_pane_hsb = {
   saturation = 0.8,
@@ -43,9 +46,10 @@ config.colors = {
   },
   split = "#b8bb26",
 }
-config.freetype_load_target = "Light"
+freetype_load_flags = "NO_HINTING"
+config.freetype_load_target = "Normal"
 config.freetype_render_target = "HorizontalLcd"
-config.font_size = 13.7
+config.font_size = 14.9
 -- config.line_height = 1.2
 config.use_cap_height_to_scale_fallback_fonts = true
 config.font = wezterm.font_with_fallback {
@@ -65,8 +69,9 @@ config.font = wezterm.font_with_fallback {
   -- 	},
   -- },
   -- {
-  -- 	family = "Monaspace Krypton",
-  -- 	-- family = "Monaspace Neon",
+  -- 	-- family = "Monaspace Krypton",
+  -- 	family = "Monaspace Argon",
+  --       scale = 1.02,
   -- 	weight = "Medium",
   -- 	italic = false,
   -- 	harfbuzz_features = {
@@ -80,64 +85,70 @@ config.font = wezterm.font_with_fallback {
   -- 		"ss08",
   -- 	},
   -- },
-  -- {
-  --   family = "Fira Code",
-  --   weight = "Medium",
-  --   italic = false,
-  --   harfbuzz_features = {
-  --     "zero",
-  --     "ss01",
-  --     "ss02",
-  --     "ss03",
-  --     "ss04",
-  --     "ss05",
-  --     "ss07",
-  --     "ss08",
-  --     "cv30",
-  --     "cv31",
-  --     "cv25",
-  --     "cv26",
-  --     "cv32",
-  --     "onum",
-  --   },
-  -- },
   {
-    family = "Cascadia Code",
+    family = "Fira Code",
     weight = "Medium",
+    -- scale = 1.04,
     italic = false,
     harfbuzz_features = {
-      "cv04",
+      "zero",
       "ss01",
       "ss02",
+      "ss03",
       "ss04",
       "ss05",
       "ss07",
-      "cv13",
-      "cv25",
-      "cv28",
+      "ss08",
       "cv30",
       "cv31",
+      "cv25",
+      "cv26",
       "cv32",
-    }
+      "onum",
+    },
   },
+  -- {
+  --   family = "Cascadia Code",
+  --   scale = 1.07,
+  --   weight = "Medium",
+  --   italic = false,
+  --   harfbuzz_features = {
+  --     "cv04",
+  --     "ss01",
+  --     "ss02",
+  --     "ss04",
+  --     "ss05",
+  --     "ss07",
+  --     "cv13",
+  --     "cv25",
+  --     "cv28",
+  --     "cv30",
+  --     "cv31",
+  --     "cv32",
+  --   }
+  -- },
   {
     family = "Symbols Nerd Font",
     weight = "Regular",
     italic = false,
   },
 }
-config.window_background_opacity = 0.9
-config.macos_window_background_blur = 30
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 25
 config.window_frame = {
   font = wezterm.font { family = "Roboto", weight = "Medium" },
   font_size = 13.0,
 }
 config.window_padding = {
-  left = 0,
+  left = 5,
   right = 0,
-  top = 0,
+  top = 10,
   bottom = 0,
 }
+-- config.window_content_alignment = {
+--   horizontal = "Center",
+--   vertical = "Center",
+-- }
 config.use_dead_keys = true
 config.audible_bell = "Disabled"
 config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 2000 }
@@ -156,10 +167,7 @@ config.keys = {
   },
   {
     key = "|",
-    -- US Keyboard
-    -- mods = "LEADER|SHIFT",
-    -- Macos M3 spanish keyboard
-    -- mods = "LEADER",
+    mods = "LEADER",
     action = act.SplitHorizontal { domain = "CurrentPaneDomain" },
   },
   {
@@ -230,19 +238,20 @@ config.window_background_gradient = {
   -- Linear and Radial gradients are also supported; see the other
   -- examples below
   -- orientation = "Vertical",
+  orientation = { Linear = { angle = 45.0 } },
   -- orientation = { Linear = { angle = -45.0 } },
-  orientation = {
-    Radial = {
-      cx = 0.8,
-      cy = 0.8,
-      radius = 1.2,
-    }
-  },
+  -- orientation = {
+  --   Radial = {
+  --     cx = 0.8,
+  --     cy = 0.8,
+  --     radius = 1.2,
+  --   }
+  -- },
 
   -- Specifies the set of colors that are interpolated in the gradient.
   -- Accepts CSS style color specs, from named colors, through rgb
   -- strings and more
-  colors = require("gradient_themes").dark_blue,
+  colors = require("gradient_themes").blue,
 
   -- Instead of specifying `colors`, you can use one of a number of
   -- predefined, preset gradients.
@@ -279,11 +288,14 @@ config.window_background_gradient = {
   -- segment_smoothness = 0.0,
 }
 wezterm.on("update-right-status", function (window, _)
+    -- local SOLID_LEFT_ARROW = ""
+    -- local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
     local prefix = ""
 
     if window:leader_is_active() then
-    -- prefix = " " .. utf8.char(0x1f30a) -- ocean wave
       prefix = "  "
+        -- prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+        -- SOLID_LEFT_ARROW = utf8.char(0xe0b2)
     end
 
     -- if window:active_tab():tab_id() ~= 0 then
